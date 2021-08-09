f3b56621cf599aec8b7553fca7ecfcf1be0625c1
Marco Betschart <email@marco.betschart.name>
2021-08-09 09:41:54 +0200

    Added comment on how to launch GTK Inspector

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ set -e
 flatpak-builder build com.github.marbetschar.time-limit.yml --user --install --force-clean
 
 export G_MESSAGES_DEBUG=all
+# Uncomment to launch GTK Inspector upon app start:
+#export GTK_DEBUG=interactive
 flatpak run com.github.marbetschar.time-limit
